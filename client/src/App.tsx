d0bb7764f58e26c35366daa441787afa8ab23c48
UT0P1C <ljonham@gmail.com>
2023-01-19 15:17:39 -0300

    Front end install and setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Habit } from "./components/Habit"
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+
+      <Habit
+      completed={4}
+      />
+    </div>
+  )
+}
+
+export default App
