339db49bc3c15fc84fb339e6f12a9917d1513cac
UT0P1C <ljonham@gmail.com>
2023-01-19 14:23:11 -0300

    Installing prisma ORM

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hello")
+import Fastify from 'fastify';
+
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const habits = prisma.habit.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 1337
+}, () => {
+  console.log('Server running on port 1337')
+})
