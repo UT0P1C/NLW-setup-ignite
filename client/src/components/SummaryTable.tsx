737c839ee7dd871a6d3b0c3607f41c6f6cc651ea
UT0P1C <ljonham@gmail.com>
2023-01-20 12:06:44 -0300

    finalizing SummaryTable component

--- client/src/components/SummaryTable.tsx
+++ client/src/components/SummaryTable.tsx
@@ -10,10 +10,9 @@ const weekDays = [
   'S',
   'S',
 ]
-
 const summaryDates = generateDatesFromYearBeginning()
-
-console.log(summaryDates)
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill =  minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable () {
   return (
@@ -41,6 +40,15 @@ export function SummaryTable () {
             )
           })
         }
+
+        {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
+          return (
+            <div 
+            key={i} 
+            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          )
+        })}
       </div>
     </div>
   )
